3bd59a48e52f7a03d6fdd1d736b15a247e7bcf5b
benakehurst <benakehurst@hotmail.com>
2020-06-25 15:32:18 +0100

    feat(stocks): add comments and tidy response

--- api/controllers/stocksController.js
+++ api/controllers/stocksController.js
@@ -5,14 +5,15 @@ const predictors = require('../../middlewares/predictors');
 const tools = require('../../middlewares/tools');
 
 /**
- * TEST METHOD
+ * Get Stock Prediction
  * GET:
+ * param: stock - stock symbol
  */
 exports.get_stock_prediction = async (req, res) => {
   const stock = req.params.stock;
 
   // Fetches Raw Stock Data
-  let rawData = await fetch(
+  const rawData = await fetch(
     `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stock}&apikey=${process.env.ALPHA_VANTAGE_KEY}&datatype=json`
   )
     .then((response) => response.json())
@@ -21,11 +22,21 @@ exports.get_stock_prediction = async (req, res) => {
     });
 
   // Converts Data for Brain.js
-  const convertedData = tools.dataConverter(rawData);
-
-  res.status(200).json({
-    success: true,
-    message: 'Data received',
-    data: convertedData,
-  });
+  const convertedData = await tools.dataConverter(rawData);
+  // Call Brain Function for prediction
+  const prediction = await predictors.makePrediction(convertedData);
+  // Response Handler
+  if (prediction) {
+    res.status(200).json({
+      success: true,
+      message: 'Prediction made successfully!',
+      data: { Prediction: prediction },
+    });
+  } else {
+    res.status(404).json({
+      success: false,
+      message: 'Prediction failed',
+      data: null,
+    });
+  }
 };
